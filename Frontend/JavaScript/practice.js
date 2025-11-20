070e42e837c1d4b21649046f6326eb72c8db2d04
Vishal Yadav <vyxwork@gmail.com>
2025-11-20 12:08:54 +0530

    string immutability

--- Frontend/JavaScript/practice.js
+++ Frontend/JavaScript/practice.js
@@ -1,6 +1,6 @@
 // node Frontend/JavaScript/practice.js
 
-let fName = "vishal";
+/* let fName = "vishal";
 let lName = "yadav";
 let fullName = fName +' '+ lName;
 console.log("Full Name:", fullName); //Full Name: vishal yadav
@@ -8,4 +8,22 @@ console.log("Full Name:", fullName); //Full Name: vishal yadav
 console.log(fName[0]) //v
 
 fName[0] = 'Y' // not allowed in string
-console.log(fName[0]) //v
+console.log(fName[0]) //v */
+
+/* immutable string example
+let str = "hello";
+console.log("Original String:", str);
+console.log("First Character:", str[0]);
+
+// trying to change the first character
+str[0] = 'H'; // This will not change the string
+console.log("After Attempting to Change First Character:", str);
+
+// correct way to change string
+let newStr = 'H' + str.slice(1);
+console.log("New String after Modification:", newStr);
+
+// Original String: hello
+// First Character: h
+// After Attempting to Change First Character: hello
+// New String after Modification: Hello */
