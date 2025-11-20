9cd53dd1c7dccfeef614622c01c6f19ab1fae226
Vishal Yadav <vyxwork@gmail.com>
2025-11-20 10:54:58 +0530

    data types

---
+++ Frontend/index.js
@@ -0,0 +1,69 @@
+// node index.js
+
+/* --- JavaScript Data Types ---
+
+1. Primitive Data Types
+-- Primitive data types are immutable (cannot be changed)
+-- string
+-- number
+-- boolean
+-- undefined
+-- null
+-- bigint
+-- symbol
+
+-- String: Represents a sequence of characters (text).
+let stringValue = "Vishal";
+console.log("String:", stringValue, typeof stringValue);
+
+-- Number: Represents both integer and floating-point numbers.
+let numberValue = 25;
+let floatValue = 99.99;
+console.log("Number:", numberValue, typeof numberValue);
+
+-- Boolean: Represents a logical entity and can have two values: true or false.
+let booleanValue = true;
+console.log("Boolean:", booleanValue, typeof booleanValue);
+
+-- Undefined: Represents a variable that has been declared but not assigned a value.
+let undefinedValue;
+console.log("Undefined:", undefinedValue, typeof undefinedValue);
+
+-- Null: Represents the intentional absence of any object value.
+let nullValue = null;
+console.log("Null:", nullValue, typeof nullValue); // Note: typeof null returns 'object' due to a legacy bug
+
+-- BigInt: Represents integers with arbitrary precision (larger than 2^53 - 1).
+let bigIntValue = 9007199254740991n;
+console.log("BigInt:", bigIntValue, typeof bigIntValue);
+
+-- Symbol: Represents a unique and immutable identifier.
+let symbolValue = Symbol("id");
+console.log("Symbol:", symbolValue.toString(), typeof symbolValue);
+
+2. Non-Primitive (Reference) Data Types
+-- Non-Primitive data types are mutable (can be changed)
+
+-- function 
+-- object
+-- array
+
+-- Object: Represents a collection of properties (key-value pairs).
+let user = {
+  name: "Vishal",
+  age: 25
+};
+console.log("Object:", user, typeof user);
+
+-- Array: A special type of object used to store ordered collections of values.
+let colors = ["Red", "Green", "Blue"];
+console.log("Array:", colors, typeof colors);
+
+-- Function: Represents a block of code that can be executed when called.
+function greet() {
+  console.log("Hello!");
+}
+console.log("Function:", greet, typeof greet);
+*/
+
+
