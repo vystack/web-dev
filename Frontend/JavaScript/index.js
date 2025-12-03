22adbe7e1e7a92ed93d1256257fe48506409ad89
Vishal Yadav <vyxwork@gmail.com>
2025-12-03 12:27:39 +0530

    getDate

--- Frontend/JavaScript/index.js
+++ Frontend/JavaScript/index.js
@@ -461,3 +461,4 @@ let now = new Date();
 console.log(now.toString());
 console.log("Year:", now.getFullYear()); // Year
 console.log("Month:", now.getMonth() + 1); // Months are zero-based
+console.log("Date:", now.getDate()); // Day of the month
