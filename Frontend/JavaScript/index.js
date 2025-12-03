74f764a587912c2d9bd81c9710b05526d744ed1f
Vishal Yadav <vyxwork@gmail.com>
2025-12-03 12:28:06 +0530

    getHours

--- Frontend/JavaScript/index.js
+++ Frontend/JavaScript/index.js
@@ -462,3 +462,4 @@ console.log(now.toString());
 console.log("Year:", now.getFullYear()); // Year
 console.log("Month:", now.getMonth() + 1); // Months are zero-based
 console.log("Date:", now.getDate()); // Day of the month
+console.log("Hours:", now.getHours()); // Hours
