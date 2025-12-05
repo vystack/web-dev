8f93c49b4f8153dd51a20cdc4c5ebfe4103ce21a
Vishal Yadav <vyxwork@gmail.com>
2025-12-05 15:34:39 +0530

    spread operator

--- Frontend/JavaScript/index.js
+++ Frontend/JavaScript/index.js
@@ -526,5 +526,8 @@ console.log(arr1.concat(arr2, arr3)); // [ 1, 2, 3, 4, 5, 6, 'vishal', 2, true ]
 let combineArr1 = arr1.concat(arr2, arr3);
 console.log(combineArr1); // [ 1, 2, 3, 4, 5, 6, 'vishal', 2, true ]
 
+// spread operator
+let combineArr2 = [...arr1, ...arr2, ...arr3];
+console.log(combineArr2); // [ 1, 2, 3, 4, 5, 6, 'vishal', 2, true ]
 
 //node index.js
